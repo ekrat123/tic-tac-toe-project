6d4b937a4e0ee8d49fa08a2391b22b74d3909343
ekrat123 <dilekmehmet123@gmail.com>
2023-09-02 14:42:45 +0300

    comments edited.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const GameBoard = (function () {
 
   const checkWinner = function () {
     const winnerCombos = [
-      // Define winning combinations
       [0, 1, 2],
       [3, 4, 5],
       [6, 7, 8],
@@ -51,60 +50,68 @@ const Player = function (name, marker) {
 const DisplayController = (function () {
   const gameBoard = document.querySelector("#game-board");
   const resetBtn = document.querySelector("#restart-button");
+  const scoreEl = document.createElement("h2");
 
-  const player1 = Player("PlayerX", "X"); // Create player 1
-  const player2 = Player("PlayerO", "O"); // Create player 2
+  const player1 = Player("Player-X", "X"); // Create player 1
+  const player2 = Player("Player-O", "O"); // Create player 2
 
   let currentPlayer = player1; // Initialize the current player as player 1
   let gameIsOver = false; // Initialize the game state as not over
 
   const createCell = function (val, i) {
-    const cellEL = document.createElement("div"); // Create a cell element
-    cellEL.classList.add("cell"); // Add a CSS class to the cell
-    cellEL.dataset.id = i; // Set the data attribute to the cell's index
-    cellEL.textContent = val; // Set the cell's content to the marker
-    return cellEL;
+    const cellEl = document.createElement("div");
+    cellEl.classList.add("cell");
+    cellEl.dataset.id = i;
+    cellEl.textContent = val;
+    return cellEl;
   };
 
   const displayBoard = () => {
-    gameBoard.innerHTML = ""; // Clear the game board
-    board = GameBoard.getBoard(); // Get the current game board
+    gameBoard.innerHTML = "";
+    board = GameBoard.getBoard();
     board.forEach((val, i) => {
-      gameBoard.appendChild(createCell(val, i)); // Create and add cells to the game board
+      gameBoard.appendChild(createCell(val, i));
     });
   };
-  displayBoard(); // Initial display of the game board
+  displayBoard();
 
   const declareWinner = (win) => {
-    const scoreEl = document.createElement("h2"); // Create an h2 element for displaying the result
     scoreEl.textContent =
-      win === "tie" ? "It is a tie" : `Player ${currentPlayer.marker} win`; // Display the appropriate message
-    gameIsOver = true; // Set the game state to over
+      win === "tie" ? "It is a tie" : `Game Over! ${currentPlayer.name} wins`;
+    gameIsOver = true;
+    document.body.appendChild(scoreEl);
+  };
 
-    document.body.appendChild(scoreEl); // Add the result message to the page
+  const declareTurn = () => {
+    changePlayer();
+    scoreEl.textContent = `${currentPlayer.name}'s turn`;
+    document.body.appendChild(scoreEl);
+  };
+
+  const changePlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
   };
 
   const clickHandler = (e) => {
     if (gameIsOver) {
-      return; // If the game is over, ignore clicks
+      return;
     }
-    const targetID = e.target.dataset.id; // Get the clicked cell's index
-    GameBoard.makeMove(targetID, currentPlayer.marker); // Make a move in the game board
-    displayBoard(); // Update the game board display
+    const targetID = e.target.dataset.id;
+    GameBoard.makeMove(targetID, currentPlayer.marker);
+    displayBoard();
 
-    winner = GameBoard.checkWinner(); // Check if there's a winner
-    winner ? declareWinner(winner) : null; // Declare the winner or continue the game
-    currentPlayer = currentPlayer === player1 ? player2 : player1; // Switch to the other player
+    winner = GameBoard.checkWinner();
+    winner ? declareWinner(winner) : declareTurn();
   };
 
   const resetGame = () => {
-    GameBoard.reset(); // Reset the game board
-    displayBoard(); // Update the game board display
-    currentPlayer = player1; // Reset the current player to player 1
-    document.querySelector("h2") ? document.querySelector("h2").remove() : null; // Remove the result message if it exists
-    gameIsOver = false; // Set the game state to not over
+    GameBoard.reset();
+    displayBoard();
+    currentPlayer = player1;
+    scoreEl ? document.querySelector("h2").remove() : null;
+    gameIsOver = false;
   };
 
-  gameBoard.addEventListener("click", clickHandler); // Add a click event listener to the game board
-  resetBtn.addEventListener("click", resetGame); // Add a click event listener to the reset button
+  gameBoard.addEventListener("click", clickHandler);
+  resetBtn.addEventListener("click", resetGame);
 })();
